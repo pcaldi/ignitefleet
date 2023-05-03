296e093c154f1746c3aa3cc3c4f2474b5cc455c3
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-03 12:42:19 -0300

    Initial commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+
+import {  Text, View } from 'react-native';
+import { styles } from './styles';
+
+
+export  function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+     
+    </View>
+  );
+}
