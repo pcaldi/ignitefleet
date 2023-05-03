4b39c4c920341da46bd98d965fc4e69fb2f20184
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-03 14:39:04 -0300

    chore: Custom Fonts + Loading Component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import {ThemeProvider} from 'styled-components/native'
-import { SignIn } from './src/screens/SignIn';
+import { useFonts,Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import theme from './src/theme';
 
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
+
 export default function App() {
+const [fontLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+if (!fontLoaded) {
+  return(
+    <Loading/>
+  );
+}
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent
+      />
       <SignIn/>
     </ThemeProvider>
   );
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+  Container,
+  LoadIndicator,
+} from './styles';
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIndicator/>
+    </Container>
+  );
+}
