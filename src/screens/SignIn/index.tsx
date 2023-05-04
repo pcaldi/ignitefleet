a4ecd8ebb65ffd82bb9cf5b73f227376d3214b3a
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-04 16:27:14 -0300

    chore: Login User

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
+import { Realm, useApp } from '@realm/react';
 
 import { Container, Slogan, Title } from './styles';
 
@@ -9,11 +10,13 @@ import backgroundImg from '../../assets/background.png';
 import { Button } from '../../components/Button';
 
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
+import { Alert } from 'react-native';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const app = useApp();
 
   const [_, response, googleSignIn] = Google.useAuthRequest({
     androidClientId: ANDROID_CLIENT_ID,
@@ -34,7 +37,16 @@ export function SignIn() {
   useEffect(() => {
     if (response?.type === 'success') {
       if (response.authentication?.idToken) {
+        const credentials = Realm.Credentials.jwt(response.authentication.idToken);
+        app.logIn(credentials).catch((error) => {
+          console.error(error);
+          Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google');
+          setIsAuthenticating(false);
+        });
       }
+    } else {
+      Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google');
+      setIsAuthenticating(false);
     }
   }, [response]);
 
