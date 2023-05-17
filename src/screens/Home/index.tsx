1c022417eac3a702db4e5f7e97d143f1c7e175ee
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-17 20:11:44 -0300

    chore: screen Home navigate

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
+
+import { CarStatus } from '../../components/CarStatus';
 import { HomeHeader } from '../../components/HomeHeader';
-import { Container } from './styles';
+
+import { Container, Content } from './styles';
 
 export function Home() {
+  const { navigate } = useNavigation();
+
+  function handleRegisterMoment() {
+    navigate('departure');
+  }
+
   return (
     <Container>
       <HomeHeader />
+      <Content>
+        <CarStatus onPress={handleRegisterMoment} />
+      </Content>
     </Container>
   );
 }
