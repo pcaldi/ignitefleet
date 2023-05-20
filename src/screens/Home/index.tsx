5d4d28de398b9dc87ab6b9ceebb9de54c493ab99
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-19 21:27:20 -0300

    chore: Displaying bank data.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,29 @@
 import { useNavigation } from '@react-navigation/native';
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
 
 import { CarStatus } from '../../components/CarStatus';
 import { HomeHeader } from '../../components/HomeHeader';
 
 import { Container, Content } from './styles';
+import { useEffect } from 'react';
 
 export function Home() {
   const { navigate } = useNavigation();
 
+  const historic = useQuery(Historic);
+
   function handleRegisterMoment() {
     navigate('departure');
   }
+
+  function fetchVehicle() {
+    console.log(historic);
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   return (
     <Container>
