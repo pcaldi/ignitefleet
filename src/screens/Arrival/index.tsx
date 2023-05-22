0fa6ed098165ef0355710e9b3daa8617739fc62e
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-22 18:51:47 -0300

    chore: Create screen Arrival

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,8 +1,9 @@
 import { useRoute } from '@react-navigation/native';
 
 import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
 
-import { Container } from './styles';
+import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
 
 type RouteParamsProps = {
   id: string;
@@ -15,6 +16,21 @@ export function Arrival() {
   return (
     <Container>
       <Header title="Chegada" />
+      <Content>
+        <Label>Placa do Veículo</Label>
+        <LicensePlate>XXX2023</LicensePlate>
+
+        <Label>Finalidade</Label>
+        <Description>
+          Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis
+          nisl.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Mais vale um
+          bebadis conhecidiss, que um alcoolatra anonimis.Delegadis gente finis, bibendum egestas
+          augue arcu ut est.
+        </Description>
+        <Footer>
+          <Button title="Registrar chegada" />
+        </Footer>
+      </Content>
     </Container>
   );
 }
