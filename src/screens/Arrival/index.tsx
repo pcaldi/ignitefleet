ecd86a033278a019b86f564c5f4e88b38065f388
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-22 19:44:17 -0300

    chore: implements remove vehicle

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,10 @@
-import { useRoute } from '@react-navigation/native';
+import { Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { X } from 'phosphor-react-native';
+
+import { BSON } from 'realm';
+import { useObject, useRealm } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -15,22 +20,42 @@ export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
 
+  const realm = useRealm();
+  const { goBack } = useNavigation();
+
+  const historic = useObject(Historic, new BSON.UUID(id));
+
+  function handleRemoveVehicleUsage() {
+    Alert.alert('Cancelar', 'Deseja cancelar a utilização do veículo?', [
+      { text: 'Não', style: 'cancel' },
+      {
+        text: 'Sim',
+        onPress: () => {
+          removeVehicleUsage();
+        },
+      },
+    ]);
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() => {
+      realm.delete(historic);
+    });
+
+    goBack();
+  }
+
   return (
     <Container>
       <Header title="Chegada" />
       <Content>
         <Label>Placa do Veículo</Label>
-        <LicensePlate>XXX2023</LicensePlate>
+        <LicensePlate>{historic?.license_plate}</LicensePlate>
 
         <Label>Finalidade</Label>
-        <Description>
-          Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis
-          nisl.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Mais vale um
-          bebadis conhecidiss, que um alcoolatra anonimis.Delegadis gente finis, bibendum egestas
-          augue arcu ut est.
-        </Description>
+        <Description>{historic?.description}</Description>
         <Footer>
-          <ButtonIcon icon={X} />
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
           <Button title="Registrar chegada" />
         </Footer>
       </Content>
