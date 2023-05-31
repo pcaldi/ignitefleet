8bc21ed803aa0cb780032c577de2f2ad758c757e
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-31 19:43:22 -0300

    chore: state percentage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,20 +2,25 @@ import { useEffect, useState } from 'react';
 import { Alert, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import dayjs from 'dayjs';
+import Toast from 'react-native-toast-message';
 
 import { useUser } from '@realm/react';
 import { useQuery, useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
+import { getLastSyncTimestamp, saveLastSyncTimestamp } from '../../libs/asyncStorage/syncStorage';
 
 import { CarStatus } from '../../components/CarStatus';
 import { HomeHeader } from '../../components/HomeHeader';
 import { HistoricCard, HistoricCardProps } from '../../components/HistoricCard';
 
 import { Container, Content, Label, Title } from './styles';
+import { TopMessage } from '../../components/TopMessage';
+import { CloudArrowUp } from 'phosphor-react-native';
 
 export function Home() {
   const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
   const [vehicleHistoric, setVehicleHistoric] = useState<HistoricCardProps[]>([]);
+  const [percentageToSync, setPercentageToSync] = useState<string | null>(null);
 
   const { navigate } = useNavigation();
   const user = useUser();
@@ -40,15 +45,17 @@ export function Home() {
     }
   }
 
-  function fetchHistoric() {
+  async function fetchHistoric() {
     try {
       const response = historic.filtered("status = 'arrival' SORT(created_at DESC)");
+
+      const lastSync = await getLastSyncTimestamp();
 
       const formattedHistoric = response.map((item) => {
         return {
           id: item._id!.toString(),
           licensePlate: item.license_plate,
-          isSync: false,
+          isSync: lastSync > item.updated_at!.getTime(),
           created: dayjs(item.created_at).format('[Saída em] DD/MM/YYYY [ás] HH:mm'),
         };
       });
@@ -62,6 +69,24 @@ export function Home() {
 
   function handleHistoricDetails(id: string) {
     navigate('arrival', { id });
+  }
+
+  async function progressNotification(transferred: number, transferable: number) {
+    const percentage = (transferred / transferable) * 100;
+
+    if (percentage === 100) {
+      await saveLastSyncTimestamp();
+      await fetchHistoric();
+      setPercentageToSync(null);
+
+      Toast.show({
+        type: 'info',
+        text1: 'Todos os dados estão sincronizados!',
+      });
+    }
+    if (percentage < 100) {
+      setPercentageToSync(`${percentage.toFixed(0)}% sincronizado.`);
+    }
   }
 
   useEffect(() => {
@@ -90,9 +115,25 @@ export function Home() {
     });
   }, [realm]);
 
+  useEffect(() => {
+    const syncSession = realm.syncSession;
+    if (!syncSession) {
+      return;
+    }
+
+    syncSession.addProgressNotification(
+      Realm.ProgressDirection.Upload,
+      Realm.ProgressMode.ReportIndefinitely,
+      progressNotification
+    );
+
+    return () => syncSession.removeProgressNotification(progressNotification);
+  }, []);
+
   return (
     <Container>
       <HomeHeader />
+      {percentageToSync && <TopMessage title={percentageToSync} icon={CloudArrowUp} />}
       <Content>
         <CarStatus licensePlate={vehicleInUse?.license_plate} onPress={handleRegisterMoment} />
 
