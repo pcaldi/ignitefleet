569b648861efc7df4b422f2e982f404ae27925a7
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-31 19:40:14 -0300

    chore: add netInfo

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { StatusBar } from 'react-native';
 import { AppProvider, UserProvider } from '@realm/react';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { WifiSlash } from 'phosphor-react-native';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 import theme from './src/theme';
 
@@ -17,9 +19,12 @@ import { Routes } from './src/routes';
 import { RealmProvider, syncConfig } from './src/libs/realm';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { TopMessage } from './src/components/TopMessage';
 
 export default function App() {
   const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  const netInfo = useNetInfo();
 
   if (!fontLoaded) {
     return <Loading />;
@@ -29,6 +34,7 @@ export default function App() {
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
         <SafeAreaProvider style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_800 }}>
+          {!netInfo.isConnected && <TopMessage title="Você está off-line." icon={WifiSlash} />}
           <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
           <UserProvider fallback={SignIn}>
             <RealmProvider sync={syncConfig} fallback={Loading}>
