cd4a998a9a2fe2f732bc3ef95ce211ca83136a4a
pcaldi <pcaldi@gmail.com>
2023-08-31 16:18:56 -0300

    feat: create start/stop task

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,5 +1,10 @@
-import {startLocationUpdatesAsync, Accuracy} from 'expo-location'
 import * as TaskManager from 'expo-task-manager';
+import {
+  Accuracy,
+  startLocationUpdatesAsync,
+  hasStartedLocationUpdatesAsync,
+  stopLocationUpdatesAsync
+} from 'expo-location'
 
 
 export const BACKGROUND_TASK_NAME = 'location-tracking';
@@ -27,12 +32,32 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({data, error}:any ) => {
 
 export async function startLocationTask() {
   try {
+    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+
+    if(hasStarted) {
+      await stopLocationTask();
+    }
 
     await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
       accuracy: Accuracy.Highest,
       distanceInterval: 1,
       timeInterval: 1000,
     })
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+export async function stopLocationTask() {
+  try {
+    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+
+    if(hasStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+    }
+
 
   } catch (error) {
     console.error(error);
