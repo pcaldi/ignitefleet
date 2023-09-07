ca631b146b4d17b9a3ce01c8119be8ee3c983d0a
pcaldi <pcaldi@gmail.com>
2023-09-07 11:18:11 -0300

    feat : save and remove storage

--- src/libs/asyncStorage/locationStorage.ts
+++ src/libs/asyncStorage/locationStorage.ts
@@ -2,6 +2,13 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const STORAGE_KEY = '@ignitefleet:location';
 
+type LocationProps = {
+  latitude: number;
+  longitude: number;
+  timestamp: number;
+}
+
+/* Método para pegar as coordenadas */
 
 export async function getStorageLocations(){
   const storage = await AsyncStorage.getItem(STORAGE_KEY);
@@ -9,4 +16,18 @@ export async function getStorageLocations(){
 
   return response;
 
+}
+ /* Método para salvar as coordenadas */
+
+export async function saveStorageLocations(newLocation: LocationProps) {
+  const storage = await getStorageLocations();
+  storage.push(newLocation);
+
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
+}
+
+/* Método para remover as coordenadas */
+
+export async function removeStorageLocations(){
+  await AsyncStorage.removeItem(STORAGE_KEY);
 }
