a2f90060c247d6d4d0e28daea2098004d8daf511
pcaldi <pcaldi@gmail.com>
2023-09-07 11:19:14 -0300

    feat: using saveStorageLocation

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -5,27 +5,32 @@ import {
   hasStartedLocationUpdatesAsync,
   stopLocationUpdatesAsync
 } from 'expo-location'
+import { removeStorageLocations, saveStorageLocations } from '../libs/asyncStorage/locationStorage';
 
 
 export const BACKGROUND_TASK_NAME = 'location-tracking';
 
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({data, error}:any ) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({data, error}:any ) => {
   try {
     if (error) {
       throw error
     }
-   const {coords, timestamp } = data.locations[0]
 
-   const currentLocation = {
-    latitude: coords.latitude,
-    longitude: coords.longitude,
-    timestamp: timestamp,
-   }
+  if (data) {
+    const {coords, timestamp } = data.locations[0]
 
-   console.log(currentLocation);
+    const currentLocation = {
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      timestamp: timestamp,
+    }
+
+    await saveStorageLocations(currentLocation);
+  }
 
   } catch (error) {
     console.error(error);
+    stopLocationTask()
   }
 })
 
@@ -46,6 +51,7 @@ export async function startLocationTask() {
 
   } catch (error) {
     console.error(error);
+
   }
 }
 
